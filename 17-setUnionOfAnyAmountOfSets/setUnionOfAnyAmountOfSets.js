b0f45fe0e3b4b91265e3b6c9b355456487d1ccfa
TJordan77 <tvon777@gmail.com>
2025-06-16 03:34:17 -0400

    Update setUnionOfAnyAmountOfSets.js

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,13 @@
 function setUnionOfAnyAmountOfSets(...args) {
-  // Insert code here;
+  const result = new Set();
+  
+  for (const set of args) {
+    for (const value of set) {
+      result.add(value);
+    }
+  }
+
+  return result;
 }
 
 // Do not edit this line;
